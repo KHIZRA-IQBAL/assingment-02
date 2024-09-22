241d91637e3ba2500482a22987525874284369f8
KHIZRA-IQBAL <iqbalkhizra026@gmail.com>
2024-09-22 17:05:18 +0500

    Your descriptive commit message

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ export default function About(){
       return(
     <div className="pb-3">
     <h1>About details</h1>
-     <p className="pt-4">Hello guys, here's some  information about myself</p>
+     <p className="pt-4">Hello guys, here some  information about myself</p>
      <h2>Education</h2>
      <p>I passed my intermediate from FG collage hyderabad Cantt ,Now i am focusing at some unbeatalble technologies </p>
      <h2 className="text-3xl">Skills</h2>
